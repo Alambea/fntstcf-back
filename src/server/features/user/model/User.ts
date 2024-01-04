7564f9d6e066e6c276887f30dac446c4593e8b58
Ana Lambea <analambeap@gmail.com>
2024-01-05 00:31:40 +0100

    Create User mongoose model schema

---
+++ src/server/features/user/model/User.ts
@@ -0,0 +1,30 @@
+import { Schema, model } from "mongoose";
+import { type UserStructure } from "../../types";
+
+const UserSchema = new Schema<UserStructure>({
+  name: {
+    type: String,
+    required: true,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  externalId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  address: {
+    type: String,
+    required: true,
+  },
+});
+
+const User = model("User", UserSchema, "users");
+
+export default User;

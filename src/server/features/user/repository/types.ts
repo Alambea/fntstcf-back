8d12695b178d972efa2e426dce4e0be9aefa0dbd
Ana Lambea <analambeap@gmail.com>
2024-01-11 17:38:30 +0100

    Add UsersMongooseRepository's method updateUser

--- src/server/features/user/repository/types.ts
+++ src/server/features/user/repository/types.ts
@@ -2,5 +2,6 @@ import { type UserWithoutId, type UserStructure } from "../../types";
 
 export interface UsersRepository {
   getUsers(): Promise<UserStructure[]>;
-  addUser(user: UserWithoutId): Promise<UserStructure>;
+  addUser(user: UserWithoutId | UserStructure): Promise<UserStructure>;
+  updateUser(user: UserWithoutId): Promise<UserStructure | void>;
 }
